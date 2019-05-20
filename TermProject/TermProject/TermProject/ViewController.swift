468a11b365c5ea6fee49dcb4ec5cdc34170e0430
YDGYDGYDG <tlsaoss@naver.com>
2019-05-20 14:58:14 +0900

    첫 푸쉬
    레이아웃

---
+++ TermProject/TermProject/TermProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TermProject
+//
+//  Created by kpugame on 2019. 5. 16..
+//  Copyright © 2019년 YDK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

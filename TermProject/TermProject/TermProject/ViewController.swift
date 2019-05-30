629c57c6cfb38c1f42e76a3f4191549d7e3adda7
YDKYDKYDK <tlsaoss@naver.com>
2019-05-30 14:57:07 +0900

    대문 이미지 추가

--- TermProject/TermProject/TermProject/ViewController.swift
+++ TermProject/TermProject/TermProject/ViewController.swift
@@ -3,9 +3,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TileImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "mblogthumb1.phinf.naver.net.jpeg")!
+        TileImage.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: image.size)
+        TileImage.image = image
+        
     }
 
 
